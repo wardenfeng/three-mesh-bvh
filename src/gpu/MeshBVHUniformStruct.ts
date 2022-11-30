2929701c6dc66c5a6ba47863c56303e201561b19
feng <908087098@qq.com>
2022-11-30 12:08:49 +0800

    补全ts类型

--- src/gpu/MeshBVHUniformStruct.ts
+++ src/gpu/MeshBVHUniformStruct.ts
@@ -1,17 +1,21 @@
-import {
+import
+{
 	DataTexture,
 	FloatType,
 	UnsignedIntType,
 	RGBAFormat,
 	RGIntegerFormat,
 	NearestFilter,
+	Texture,
 } from 'three';
-import {
+import
+{
 	FloatVertexAttributeTexture,
 	UIntVertexAttributeTexture,
 } from './VertexAttributeTexture';
 import { BYTES_PER_NODE } from '../core/Constants';
-import {
+import
+{
 	BOUNDING_DATA_INDEX,
 	COUNT,
 	IS_LEAF,
@@ -20,58 +24,64 @@ import {
 	SPLIT_AXIS,
 } from '../core/nodeBufferFunctions';
 
-function bvhToTextures( bvh, boundsTexture, contentsTexture ) {
+function bvhToTextures(bvh: any, boundsTexture: Texture, contentsTexture: Texture)
+{
 
 	const roots = bvh._roots;
 
-	if ( roots.length !== 1 ) {
+	if (roots.length !== 1)
+	{
 
-		throw new Error( 'MeshBVHUniformStruct: Multi-root BVHs not supported.' );
+		throw new Error('MeshBVHUniformStruct: Multi-root BVHs not supported.');
 
 	}
 
-	const root = roots[ 0 ];
-	const uint16Array = new Uint16Array( root );
-	const uint32Array = new Uint32Array( root );
-	const float32Array = new Float32Array( root );
+	const root = roots[0];
+	const uint16Array = new Uint16Array(root);
+	const uint32Array = new Uint32Array(root);
+	const float32Array = new Float32Array(root);
 
 	// Both bounds need two elements per node so compute the height so it's twice as long as
 	// the width so we can expand the row by two and still have a square texture
 	const nodeCount = root.byteLength / BYTES_PER_NODE;
-	const boundsDimension = 2 * Math.ceil( Math.sqrt( nodeCount / 2 ) );
-	const boundsArray = new Float32Array( 4 * boundsDimension * boundsDimension );
+	const boundsDimension = 2 * Math.ceil(Math.sqrt(nodeCount / 2));
+	const boundsArray = new Float32Array(4 * boundsDimension * boundsDimension);
 
-	const contentsDimension = Math.ceil( Math.sqrt( nodeCount ) );
-	const contentsArray = new Uint32Array( 2 * contentsDimension * contentsDimension );
+	const contentsDimension = Math.ceil(Math.sqrt(nodeCount));
+	const contentsArray = new Uint32Array(2 * contentsDimension * contentsDimension);
 
-	for ( let i = 0; i < nodeCount; i ++ ) {
+	for (let i = 0; i < nodeCount; i++)
+	{
 
 		const nodeIndex32 = i * BYTES_PER_NODE / 4;
 		const nodeIndex16 = nodeIndex32 * 2;
-		const boundsIndex = BOUNDING_DATA_INDEX( nodeIndex32 );
-		for ( let b = 0; b < 3; b ++ ) {
+		const boundsIndex = BOUNDING_DATA_INDEX(nodeIndex32);
+		for (let b = 0; b < 3; b++)
+		{
 
-			boundsArray[ 8 * i + 0 + b ] = float32Array[ boundsIndex + 0 + b ];
-			boundsArray[ 8 * i + 4 + b ] = float32Array[ boundsIndex + 3 + b ];
+			boundsArray[8 * i + 0 + b] = float32Array[boundsIndex + 0 + b];
+			boundsArray[8 * i + 4 + b] = float32Array[boundsIndex + 3 + b];
 
 		}
 
-		if ( IS_LEAF( nodeIndex16, uint16Array ) ) {
+		if (IS_LEAF(nodeIndex16, uint16Array))
+		{
 
-			const count = COUNT( nodeIndex16, uint16Array );
-			const offset = OFFSET( nodeIndex32, uint32Array );
+			const count = COUNT(nodeIndex16, uint16Array);
+			const offset = OFFSET(nodeIndex32, uint32Array);
 
 			const mergedLeafCount = 0xffff0000 | count;
-			contentsArray[ i * 2 + 0 ] = mergedLeafCount;
-			contentsArray[ i * 2 + 1 ] = offset;
+			contentsArray[i * 2 + 0] = mergedLeafCount;
+			contentsArray[i * 2 + 1] = offset;
 
-		} else {
+		} else
+		{
 
-			const rightIndex = 4 * RIGHT_NODE( nodeIndex32, uint32Array ) / BYTES_PER_NODE;
-			const splitAxis = SPLIT_AXIS( nodeIndex32, uint32Array );
+			const rightIndex = 4 * RIGHT_NODE(nodeIndex32, uint32Array) / BYTES_PER_NODE;
+			const splitAxis = SPLIT_AXIS(nodeIndex32, uint32Array);
 
-			contentsArray[ i * 2 + 0 ] = splitAxis;
-			contentsArray[ i * 2 + 1 ] = rightIndex;
+			contentsArray[i * 2 + 0] = splitAxis;
+			contentsArray[i * 2 + 1] = rightIndex;
 
 		}
 
@@ -103,9 +113,16 @@ function bvhToTextures( bvh, boundsTexture, contentsTexture ) {
 
 }
 
-export class MeshBVHUniformStruct {
+export class MeshBVHUniformStruct
+{
+	autoDispose: boolean;
+	index: UIntVertexAttributeTexture;
+	position: FloatVertexAttributeTexture;
+	bvhBounds: DataTexture;
+	bvhContents: DataTexture;
 
-	constructor() {
+	constructor()
+	{
 
 		this.autoDispose = true;
 		this.index = new UIntVertexAttributeTexture();
@@ -117,25 +134,27 @@ export class MeshBVHUniformStruct {
 
 	}
 
-	updateFrom( bvh ) {
+	updateFrom(bvh: any)
+	{
 
 		const { geometry } = bvh;
 
-		bvhToTextures( bvh, this.bvhBounds, this.bvhContents );
+		bvhToTextures(bvh, this.bvhBounds, this.bvhContents);
 
-		this.index.updateFrom( geometry.index );
-		this.position.updateFrom( geometry.attributes.position );
+		this.index.updateFrom(geometry.index);
+		this.position.updateFrom(geometry.attributes.position);
 
 	}
 
-	dispose() {
+	dispose()
+	{
 
 		const { index, position, bvhBounds, bvhContents } = this;
 
-		if ( index ) index.dispose();
-		if ( position ) position.dispose();
-		if ( bvhBounds ) bvhBounds.dispose();
-		if ( bvhContents ) bvhContents.dispose();
+		if (index) index.dispose();
+		if (position) position.dispose();
+		if (bvhBounds) bvhBounds.dispose();
+		if (bvhContents) bvhContents.dispose();
 
 	}
 
