6110ded0b7abd398b68bb9b82403080c66425a26
feng <908087098@qq.com>
2022-12-06 15:12:34 +0800

    getHtmlNamesObject

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,15 +1,31 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import fs from 'fs';
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-				asyncGenerate: resolve(__dirname, 'asyncGenerate.html'),
-				characterMovement: resolve(__dirname, 'characterMovement.html')
-			}
+			input: getHtmlNamesObject(),
 		}
 	}
 })
+
+function getHtmlNamesObject()
+{
+	const obj = fs.readdirSync('./').reduce((pv, cv) =>
+	{
+		const ps = cv.split('.');
+
+		if (ps[1] === 'html')
+		{
+			pv[ps[0]] = resolve(__dirname, cv);
+		}
+
+		return pv;
+	}, {})
+
+	return obj;
+}
+
+
